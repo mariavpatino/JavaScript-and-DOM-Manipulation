15180999123bcba1f97b771839ea1a89fae0cf57
maria patino <mariavpatino@hotmail.com>
2020-02-15 23:59:17 -0500

    Updated

---
+++ UFO-level-1/StarterCode/static/js/app.js
@@ -0,0 +1,39 @@
+// from data.js
+var tableData = data;
+
+// Creating table and adding info to the rows
+d3.select("tbody")
+  .selectAll("tr")
+  .data(tableData)
+  .enter()
+  .append("tr")
+  .html(function(d) {
+    return `<td>${d.datetime}</td><td>${d.city}</td><td>${d.state}</td><td>${d.country}</td><td>${d.shape}</td><td>${d.durationMinutes}</td><td>${d.comments}</td>`;
+  });
+
+
+// Select the button
+var filterButton = d3.selectAll("button");
+
+// Clicking button
+filterButton.on("click", function myFunction() {
+    // Declare variables
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("datetime");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("ufo-table");
+    tr = table.getElementsByTagName("tr");
+  
+    // Loop through all table rows, and hide those who don't match the search query
+    for (i = 0; i < tr.length; i++) {
+      td = tr[i].getElementsByTagName("td")[0];
+      if (td) {
+        txtValue = td.textContent || td.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+          tr[i].style.display = "";
+        } else {
+          tr[i].style.display = "none";
+        }
+      }
+    }
+  });
